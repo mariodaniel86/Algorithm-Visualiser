b335aa5f7d458bad8883be075479816e2096bcf9
Mario Daniel <mariodanielalmrawi@gmail.com>
2025-08-02 13:13:29 +0300

    Implement depth-first search algorithm
    Since the implementation is straightforward and follows standard DFS patterns, no additional
    explanation is needed in the commit message body.

--- src/main/java/av/models/algorithms/search/DFS.java
+++ src/main/java/av/models/algorithms/search/DFS.java
@@ -1,5 +1,9 @@
 package av.models.algorithms.search;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
 import av.models.structure.*;
 
 import lombok.Getter;
@@ -8,12 +12,23 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DFS implements SearchStrategy {
+    private Map<Node, VisitState> visitStates = new HashMap<>();
+
     @Override
     public void search(Node startNode) {
         runDFS(startNode);
     }
 
     private void runDFS(Node node) {
-
+        visitStates.put(node, VisitState.IN_PROGRESS);
+        for (Edge edge : node.getEgdes()) {
+            List<Node> adjacentNodes = edge.getNodes();
+            Node neighbor = adjacentNodes.get(0).equals(node) ? adjacentNodes.get(1) : adjacentNodes.get(0);
+            VisitState nodeState = visitStates.getOrDefault(neighbor, VisitState.NOT_VISITED);
+            if (nodeState == VisitState.NOT_VISITED) {
+                runDFS(neighbor);
+            }
+        }
+        visitStates.put(node, VisitState.VISITED);
     }
 }
---
+++ src/main/java/av/models/algorithms/search/VisitState.java
@@ -0,0 +1,7 @@
+package av.models.algorithms.search;
+
+public enum VisitState {
+    NOT_VISITED,
+    IN_PROGRESS,
+    VISITED
+}
